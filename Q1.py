5432edcfba716951c60bf396a125eb7b90590160
Tania Liao <TaniaLiao@MacBook-Pro-642.local>
2021-05-25 19:00:38 +0800

    add test

--- Q1.py
+++ Q1.py
@@ -7,3 +7,20 @@ def f(si):
             s = c + s
         so += s + " "
     return so[:-1]
+
+def test(si, so):
+    if len(si) != len(so):
+        print("Invalid")
+        return False
+    Li = si.split()
+    Lo = so.split()
+    for i in range(len(si)):
+        if len(Li[s]) != len(Lo[s]):
+            print("Invalid")
+            return False
+        for j in range(len(Li[s])):
+            if Li[s][j] != Lo[s][-1-j]:
+                print("Invalid")
+                return False
+    print("Valid")
+    return True
